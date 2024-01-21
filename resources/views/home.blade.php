dfa053d86edd67e3dd8333f8ff95950feeb17162
PinoGabriel <pinogabriel01@gmail.com>
2024-01-21 11:29:02 +0100

    feat: added jumboFooter + index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,14 +28,18 @@
     </section>
 
     <section id="jumboFooter">
-        <div class="container">
+        <div class="container myHeight">
             <div class="logoFooter">
-                @foreach ($index as $indexFooter)
-                    <h4>{{ $indexFooter['title'] }}</h4>
-                    @foreach ($indexFooter['i'] as $item)
-                        <a> {{$item}} </a>
+                <div class="d-flex flex-column flex-wrap myW40 myHeightLogo py-3">
+                    @foreach ($index as $indexFooter)
+                    <div class="d-flex flex-column">
+                        <h4 class="pb-3">{{ $indexFooter['title'] }}</h4>
+                        @foreach ($indexFooter['i'] as $item)
+                            <a> {{$item}} </a>
+                        @endforeach
+                    </div>
                     @endforeach
-                @endforeach
+                </div>
             </div>
         </div>
     </section>
